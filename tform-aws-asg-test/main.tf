25d383200477b694ecacf3cd9215b8c0844df57a
nallanisai <nallani.sai2000@gmail.com>
2019-01-03 17:38:50 +0000

    Adding proper ssh keys

--- tform-aws-asg-test/main.tf
+++ tform-aws-asg-test/main.tf
@@ -23,11 +23,18 @@ resource "aws_security_group" "terraform_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+// Set up deploy SSH key
+resource "aws_key_pair" "aws_asg_ssh_key" {
+  key_name   = "aws_asg_ssh_key"
+  public_key = "${file("/ec2_user_ssh_keys/id_rsa.pub")}"
+}
+
 resource "aws_instance" "terraform-centos" {
   ami = "ami-4543be3d"
   instance_type = "t1.micro"
   vpc_security_group_ids = ["${aws_security_group.terraform_sg.id}"]
-  key_name = "terraform-instance"
+  key_name = "${aws_key_pair.aws_asg_ssh_key.key_name}"
   tags {
     Name = "tf-instance02"
   }
